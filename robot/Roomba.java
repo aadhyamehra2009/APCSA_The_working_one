b9496898160a9049690a7a0c136c23c7fc8962f3
aadhyamehra2009 <am2670@pleasantonusd.net>
2025-09-04 22:08:13 +0000

    picked up few beepers

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -16,32 +16,47 @@ public class Roomba implements Directions {
 	}
 
 	// declared here so it is visible in all the methods!
-	private Robot roomba;
+	private Robot roomba = new Robot(7, 7, North, 100);
 
 	// You will need to add many variables!!
 
-
 	public int cleanRoom(String worldName, int startX, int startY) {
 
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
-        // Make sure it starts at startX and startY location.
+		// A new Robot should be constructed and assigned to the global (instance)
+		// variable named roomba that is declared above.
+		// Make sure it starts at startX and startY location.
 
 		World.readWorld(worldName);
 		World.setVisible(true);
 
-
-		/** This section will have all the logic that takes the Robot to every location
+		/**
+		 * This section will have all the logic that takes the Robot to every location
 		 * and cleans up all piles of beepers. Think about ways you can break this
 		 * large, complex task into smaller, easier to solve problems.
 		 */
 
 		// the line below causes a null pointer exception
 		// what is that and why are we getting it?
-		roomba.move();
-
+		int j=1;
+		while (j<=8) {
+			int i = 1;
+			while (i <= 4) {
+				roomba.move();
+				while (roomba.nextToABeeper()) {
+					roomba.pickBeeper();
+				}
+				i++;
+			}
+			roomba.turnLeft();
+			roomba.turnLeft();
+			roomba.turnLeft();
+			roomba.move();
+			j++;
+			
+		}
 
 		int totalBeepers = 0; // Need to move this somewhere else.
-        // This method should return the total number of beepers cleaned up.
+		// This method should return the total number of beepers cleaned up.
 		return totalBeepers;
 	}
 }
